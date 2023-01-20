484180120703697932526158a34a2048f72a8d00
Alex_Nole <snole7@gmail.com>
2023-01-20 11:20:47 -0500

    login

--- Frontend/front-notes/src/components/ListNotes.js
+++ Frontend/front-notes/src/components/ListNotes.js
@@ -13,8 +13,8 @@ export const ListNotes = () => {
             method: 'GET',
             withCredentials: true,
             url: `${API}/notes`,
-            headers: {'Bearer': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IlNhbnRpYWdvIiwiaWF0IjoxNjc0MjI2MjQ2LCJleHAiOjE2NzQyNjIyNDZ9.kET59Fdkt0P4ltqy6HtMhjxLlC35heaAbHYysf5FiKE',
-            'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IlNhbnRpYWdvIiwiaWF0IjoxNjc0MjI2MjQ2LCJleHAiOjE2NzQyNjIyNDZ9.kET59Fdkt0P4ltqy6HtMhjxLlC35heaAbHYysf5FiKE`}
+            headers: {'Bearer': `${token}`,
+            'Authorization': `Bearer ${token}`}
         }).then((response) => {
             console.log(response)
         }).catch((error) => {
